56fab06b8fb7841c65d781e974a021218008d64f
hgncom <johan@Johans-MacBook-Pro.local>
2025-05-06 14:59:14 +0200

    Add Google site verification meta tag to _document.js for universal coverage

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Search Console Verification */}
+        <meta name="google-site-verification" content="sN6PbI5xAl9w5Mw0202LwMrmGDZCBbkcr6UgdWLYupI" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
